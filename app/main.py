ab14fa275b89a1ad2bd59dcc98e54af2d755471a
DeveloperNichols <nicolassantos19053@gmail.com>
2025-05-25 22:55:20 -0300

    feat: Cors Authorization

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import router
 
 app = FastAPI()
+
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Você pode trocar "*" pelo domínio do seu frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # Isso permite POST, GET, OPTIONS, etc.
+    allow_headers=["*"],
+)
 
 # Inclui as rotas da API
 app.include_router(router)
